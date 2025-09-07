8e6f79983203bcdc3a7017690b9457c76625fc32
Aman <zbat37@gmail.com>
2025-09-08 01:01:09 +0530

    ccc

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/react-web-tester-2/", // <-- your GitHub repo name
+  base: "/", // <-- your GitHub repo name
   plugins: [react()],
 });
